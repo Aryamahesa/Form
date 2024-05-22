96c3ef2f38c5968bee233970b981c314be2d36cd
aryamahesa <130587161+Aryamahesa@users.noreply.github.com>
2024-05-23 02:30:49 +0700

    Fix : port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require('path');
 const rootDir = require('./util/path');
 const formRouter = require('./routes/Form');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
